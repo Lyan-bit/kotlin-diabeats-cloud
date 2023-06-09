33e6c8399a3ad03bd77aa3a6c76813c295e9939a
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-09 15:12:21 +0100

    Update Ocl.java

--- app/src/main/java/com/example/diabeats/Ocl.java
+++ app/src/main/java/com/example/diabeats/Ocl.java
@@ -12,10 +12,14 @@ package com.example.diabeats; // Place in the relevant package directory of your
 /* OCL library for Java version 8+ */
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Ocl {
+ 
+ private Ocl() {
+        // Private constructor to prevent instantiation
+ }
+ 
  public static List<String> tokeniseCSV(String line)  {
    // Assumes the separator is a comma
    StringBuilder buff = new StringBuilder();
