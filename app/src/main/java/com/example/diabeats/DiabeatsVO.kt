8b43e3c1aad9a33b8725f5775ff803450696cd80
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-09 15:38:39 +0100

    Update DiabeatsVO.kt

--- app/src/main/java/com/example/diabeats/DiabeatsVO.kt
+++ app/src/main/java/com/example/diabeats/DiabeatsVO.kt
@@ -4,62 +4,66 @@ import java.util.ArrayList
 
 class DiabeatsVO  {
 
-    private var id: String = ""
-    private var pregnancies: Int = 0
-    private var glucose: Int = 0
-    private var bloodPressure: Int = 0
-    private var skinThickness: Int = 0
-    private var insulin: Int = 0
-    private var bmi: Double = 0.0
-    private var diabetesPedigreeFunction: Double = 0.0
-    private var age: Int = 0
-    private var outcome: String = ""
+        var id: String = ""
+     var age: Int = 0
+     var bmi: Float = 0.0F
+     var glucose: Float = 0.0F
+     var insulin: Float = 0.0F
+     var homa: Float = 0.0F
+     var leptin: Float = 0.0F
+     var adiponectin: Float = 0.0F
+     var resistin: Float = 0.0F
+     var mcp: Float = 0.0F
+     var outcome: String = ""
 
     constructor() {
     	//constructor
     }
 
     constructor(idx: String, 
-        pregnanciesx: Int, 
-        glucosex: Int, 
-        bloodPressurex: Int, 
-        skinThicknessx: Int, 
-        insulinx: Int, 
-        bmix: Double, 
-        diabetesPedigreeFunctionx: Double, 
         agex: Int, 
+        bmix: Float, 
+        glucosex: Float, 
+        insulinx: Float, 
+        homax: Float, 
+        leptinx: Float, 
+        adiponectinx: Float, 
+        resistinx: Float, 
+        mcpx: Float, 
         outcomex: String
         ) {
         this.id = idx
-        this.pregnancies = pregnanciesx
-        this.glucose = glucosex
-        this.bloodPressure = bloodPressurex
-        this.skinThickness = skinThicknessx
-        this.insulin = insulinx
-        this.bmi = bmix
-        this.diabetesPedigreeFunction = diabetesPedigreeFunctionx
         this.age = agex
+        this.bmi = bmix
+        this.glucose = glucosex
+        this.insulin = insulinx
+        this.homa = homax
+        this.leptin = leptinx
+        this.adiponectin = adiponectinx
+        this.resistin = resistinx
+        this.mcp = mcpx
         this.outcome = outcomex
     }
 
-    constructor (x: Diabeats) {
+    constructor (x: BreastCancer) {
         id = x.id
-        pregnancies = x.pregnancies
-        glucose = x.glucose
-        bloodPressure = x.bloodPressure
-        skinThickness = x.skinThickness
-        insulin = x.insulin
-        bmi = x.bmi
-        diabetesPedigreeFunction = x.diabetesPedigreeFunction
         age = x.age
+        bmi = x.bmi
+        glucose = x.glucose
+        insulin = x.insulin
+        homa = x.homa
+        leptin = x.leptin
+        adiponectin = x.adiponectin
+        resistin = x.resistin
+        mcp = x.mcp
         outcome = x.outcome
     }
 
     override fun toString(): String {
-        return "id = $id,pregnancies = $pregnancies,glucose = $glucose,bloodPressure = $bloodPressure,skinThickness = $skinThickness,insulin = $insulin,bmi = $bmi,diabetesPedigreeFunction = $diabetesPedigreeFunction,age = $age,outcome = $outcome"
+        return "id = $id,age = $age,bmi = $bmi,glucose = $glucose,insulin = $insulin,homa = $homa,leptin = $leptin,adiponectin = $adiponectin,resistin = $resistin,mcp = $mcp,outcome = $outcome"
     }
 
-    fun toStringList(list: List<DiabeatsVO>): List<String> {
+    fun toStringList(list: List<BreastCancerVO>): List<String> {
         val res: MutableList<String> = ArrayList()
         for (i in list.indices) {
             res.add(list[i].toString())
@@ -67,85 +71,5 @@ class DiabeatsVO  {
         return res
     }
     
-    fun getId(): String {
-        return id
-    }
-    
-    fun getPregnancies(): Int {
-        return pregnancies
-    }
-    
-    fun getGlucose(): Int {
-        return glucose
-    }
-    
-    fun getBloodPressure(): Int {
-        return bloodPressure
-    }
-    
-    fun getSkinThickness(): Int {
-        return skinThickness
-    }
-    
-    fun getInsulin(): Int {
-        return insulin
-    }
-    
-    fun getBmi(): Double {
-        return bmi
-    }
-    
-    fun getDiabetesPedigreeFunction(): Double {
-        return diabetesPedigreeFunction
-    }
-    
-    fun getAge(): Int {
-        return age
-    }
-    
-    fun getOutcome(): String {
-        return outcome
-    }
-    
-
-    fun setId(x: String) {
-    	id = x
-    }
-    
-    fun setPregnancies(x: Int) {
-    	pregnancies = x
-    }
-    
-    fun setGlucose(x: Int) {
-    	glucose = x
-    }
-    
-    fun setBloodPressure(x: Int) {
-    	bloodPressure = x
-    }
-    
-    fun setSkinThickness(x: Int) {
-    	skinThickness = x
-    }
-    
-    fun setInsulin(x: Int) {
-    	insulin = x
-    }
-    
-    fun setBmi(x: Double) {
-    	bmi = x
-    }
-    
-    fun setDiabetesPedigreeFunction(x: Double) {
-    	diabetesPedigreeFunction = x
-    }
-    
-    fun setAge(x: Int) {
-    	age = x
-    }
-    
-    fun setOutcome(x: String) {
-    	outcome = x
-    }
     
 }
